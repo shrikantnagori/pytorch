43ab1618a8b2f1e7f14d583380af09f40c26a7a0
Douglas Lehr <Doug.Lehr@amd.com>
2023-02-06 14:56:27 -0500

    [ROCm] Remove checks that disable inductor/dynamo if ROCM is detected
    * Allow test_torchinductor.py to run if TEST_WITH_ROCM is enabled
    * Enabling few dynamo UTs
    * Enable Inductor UT on ROCm
    
    [ROCm] Add ability to skip rocm ops for inductor. (#1159)
    
    Initially, not all pytorch ops will have ROCm support for inductor.  This
    provides a mechanism to allow a check for additionally skipped ops that are
    supported on CUDA but not ROCm.
    
    [ROCm] Disables cudagraph on some UTs in test_torchinductor for ROCm (#1176)
    
    [ROCm] Skipping inductor UT's caused by Triton functionality gaps
    
    Fixed linting in various files
    
    skipIfRocm in inductor/test_pattern_matcher UT
    
    Fix torch import in verify_dynamo.py
    
    skip on ROCm for Tensor-likes are not close issue
    
    Use patch_all() cudagraph test to skip on ROCm
    
    Skipping some UTs on ROCm
    
    Use BUILD_ENVIRONMENT to check rocm for install_triton
    
    Add --continue-through-error flag temporarily to capture full failure set
    
    Don't delete individual logs for test suites

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -37,11 +37,11 @@ from torch.testing._internal.common_utils import (
     IS_WINDOWS,
     IS_X86,
     TEST_WITH_ASAN,
-    TEST_WITH_ROCM,
     TestCase as TorchTestCase,
 )
 from torch.utils._python_dispatch import TorchDispatchMode
 from torch.utils._pytree import tree_flatten, tree_unflatten
+from torch.testing._internal.common_utils import skipIfRocm
 
 if IS_WINDOWS and IS_CI:
     sys.stderr.write(
@@ -2460,6 +2460,7 @@ class CommonTemplate:
             (torch.randn([4, 4, 4]),),
         )
 
+    @skipIfRocm
     def test_convolution1(self):
         m = torch.nn.Sequential(
             torch.nn.Conv2d(5, 6, [3, 3]),
@@ -2981,6 +2982,7 @@ class CommonTemplate:
             (torch.randn([10, 4]), torch.randint(10, [8]), torch.tensor([0, 2, 6])),
         )
 
+    @skipIfRocm
     def test_batch_norm_2d(self):
         m = torch.nn.Sequential(
             torch.nn.BatchNorm2d(10),
@@ -3686,6 +3688,7 @@ class CommonTemplate:
                 ),
             )
 
+    @skipIfRocm
     def test_cudnn_rnn(self):
         if self.device == "cpu":
             raise unittest.SkipTest("requires CUDA")
@@ -4090,7 +4093,7 @@ class CommonTemplate:
         inputs = (rand_strided((8,), (1,), device=self.device),)
         self.assertTrue(same(fn(*inputs), 2 * inputs[0]))
 
-    @config.patch({"triton.cudagraphs": True})
+    @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
     def test_strided_inputs(self):
         @torch._dynamo.optimize("inductor")
         def fn(x, y):
@@ -4102,7 +4105,7 @@ class CommonTemplate:
         )
         self.assertTrue(same(fn(*inputs), inputs[0] + inputs[1]))
 
-    @config.patch({"triton.cudagraphs": True})
+    @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
     @patch.object(functorch_config, "use_fake_tensor", True)
     def test_input_mutation1(self):
         def fn(a):
@@ -4674,6 +4677,7 @@ class CommonTemplate:
             ],
         )
 
+    @skipIfRocm
     def test_scatter3(self):
         def fn(a, dim, index, b):
             return aten.scatter(a, dim, index, b, reduce="add")
@@ -4759,6 +4763,7 @@ class CommonTemplate:
             ],
         )
 
+    @skipIfRocm
     def test_scatter_add2(self):
         def fn(a, dim, index, b):
             return aten.scatter_add(a, dim, index, b)
@@ -4773,6 +4778,7 @@ class CommonTemplate:
             ],
         )
 
+    @skipIfRocm
     def test_scatter_add3(self):
         def fn(a, dim, index, b):
             return aten.scatter_add(a, dim, index, b)
@@ -4789,6 +4795,7 @@ class CommonTemplate:
                     ],
                 )
 
+    @skipIfRocm
     def test_scatter_reduce1(self):
         def fn(a, dim, index, b):
             return aten.scatter_reduce(a, dim, index, b, "sum")
@@ -4803,6 +4810,7 @@ class CommonTemplate:
             ],
         )
 
+    @skipIfRocm
     def test_scatter_reduce2(self):
         def fn(a, dim, index, b):
             return aten.scatter_reduce(a, dim, index, b, "sum", include_self=False)
@@ -4895,7 +4903,7 @@ class CommonTemplate:
 
         self.common(fn2, [torch.randn(55)])
 
-    @config.patch({"triton.cudagraphs": True})
+    @config.patch({"triton.cudagraphs": True if not torch.version.hip else False})
     def test_dropout(self):
         random.seed(1234)
         torch.manual_seed(1234)
@@ -4925,7 +4933,7 @@ class CommonTemplate:
         def fn(a):
             return torch.nn.functional.dropout(a, 0.55, True)
 
-        for cg in (False, True):
+        for cg in [False, True] if not torch.version.hip else [False]:
             with patch.object(config.triton, "cudagraphs", cg):
                 torch._dynamo.reset()
 
@@ -5357,6 +5365,7 @@ class CommonTemplate:
             ],
         )
 
+    @skipIfRocm
     def test_argmax_argmin2(self):
         def fn(x):
             return (
@@ -5676,7 +5685,7 @@ class CommonTemplate:
         else:
             contexts = [
                 contextlib.nullcontext,
-                lambda: config.patch({"triton.cudagraphs": True}),
+                lambda: config.patch({"triton.cudagraphs": True if not torch.version.hip else False}),
             ]
 
         for context in contexts:
@@ -6151,6 +6160,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertEqual(arguments_that_are_divisible_by_16_in_kernel1, (0, 1))
             torch._dynamo.reset()
 
+        @skipIfRocm
         def test_optimize_indexing_dtype(self):
             def fn(x: torch.Tensor) -> torch.Tensor:
                 return aten.upsample_bilinear2d.vec(x, None, True, [2.0, 2.0])
@@ -6180,6 +6190,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
             self.assertFalse("out_ptr0" in code)
             self.assertEqual(fn_opt(*inps), fn(*inps))
 
+        @skipIfRocm
         def test_cant_optimize_compute(self):
             def ones():
                 return torch.ones([4], device="cuda")
@@ -6206,6 +6217,7 @@ if HAS_CUDA and not TEST_WITH_ASAN:
                 self.assertTrue("to(tl.int64)" in code)
                 self.assertEqual(fn_opt(), fn())
 
+        @skipIfRocm
         def test_optimize_compute(self):
             def ones():
                 return torch.ones([4], device="cuda")
@@ -6385,5 +6397,5 @@ if HAS_CPU:
 if __name__ == "__main__":
     from torch._dynamo.test_case import run_tests
 
-    if (HAS_CPU or HAS_CUDA) and not TEST_WITH_ROCM:
+    if (HAS_CPU or HAS_CUDA):
         run_tests(needs="filelock")
