e832ebc6e4405b21088a1a2e61d30954319bc21f
William Wen <williamwen@fb.com>
2023-04-12 19:29:28 +0000

    [dynamo 3.11] implement 3.11 exceptiontable
    ghstack-source-id: 8aefffd5f52d612f6f89a97360baef5156e56a4d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/96511

--- torch/_dynamo/bytecode_analysis.py
+++ torch/_dynamo/bytecode_analysis.py
@@ -35,12 +35,15 @@ def remove_dead_code(instructions):
                 return
             live_code.add(i)
             inst = instructions[i]
+            if inst.exn_tab_entry:
+                find_live_code(indexof[id(inst.exn_tab_entry.target)])
             if inst.opcode in JUMP_OPCODES:
                 find_live_code(indexof[id(inst.target)])
             if inst.opcode in TERMINAL_OPCODES:
                 return
 
     find_live_code(0)
+    # TODO change exception table entries if start/end instructions are dead
     return [inst for i, inst in enumerate(instructions) if i in live_code]
 
 
